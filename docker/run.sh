8d6e17b877d1e667387479244c4f8ab8987c0090
csantosm <4a.santos@gmail.com>
2020-12-02 10:16:40 +0100

    Updated run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -5,4 +5,5 @@ npm install && npm run build
 popd
 pushd ../
 docker build -f docker/Dockerfile -t openvidu/openvidu-virtual-background-tool .
+docker run -d -v /opt/openvidu-virtual-background-tool/recordings/:/opt/openvidu-virtual-background-tool/recordings/ -p 5000:5000 openvidu/openvidu-virtual-background-tool
 
